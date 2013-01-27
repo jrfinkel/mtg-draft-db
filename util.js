d4227eabd6835796a5334dd4089abc5512c4c0e2
Jenny Rose Finkel <jrfinkel@gmail.com>
2013-01-26 16:42:13 -0800

    blah

--- util.js
+++ util.js
@@ -38,3 +38,13 @@ exports.readGetData = function readGetData(request) {
 
     return qs.parse(url.parse(request.url).query);
 }
+
+function htmlEncode(value){
+  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+  //then grab the encoded contents back out.  The div never exists on the page.
+  return $('<div/>').text(value).html();
+}
+
+function htmlDecode(value){
+  return $('<div/>').html(value).text();
+}
