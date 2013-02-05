046d368580249b7c70293be468b3a9cc4a043afe
Jenny Rose Finkel <jrfinkel@gmail.com>
2013-02-05 02:15:19 -0800

    blah

--- drafts.js
+++ drafts.js
@@ -361,7 +361,7 @@ function confirmedStep (body, response) {
     Object.keys(data.players).forEach(function(id) {
 	var player = data.players[id];
 	console.log('PLAYER: ' + JSON.stringify(player));
-	var q = 'UPDATE players SET (set_credit, rating, ind_wins, ind_losses, draft_wins, draft_ties, draft_losses, money, latest_timestamp) = ('+ player.set_credit+', '+player.rating +', '+ player.ind_wins + ', ' + player.ind_losses + ', ' + player.draft_wins + ', ' + player.draft_ties + ', ' + player.draft_losses + ', ' + player.money +', '+util.getTS()') WHERE id = '+id+';';
+	var q = 'UPDATE players SET (set_credit, rating, ind_wins, ind_losses, draft_wins, draft_ties, draft_losses, money, latest_timestamp) = ('+ player.set_credit+', '+player.rating +', '+ player.ind_wins + ', ' + player.ind_losses + ', ' + player.draft_wins + ', ' + player.draft_ties + ', ' + player.draft_losses + ', ' + player.money +', '+util.getTS()+') WHERE id = '+id+';';
 	console.log('About to query: ' + q);
 	query = client.query(q);
     });
