88a926eb396353dcbd1a4f5afb265f1cbce1cecf
Jenny Rose Finkel <jrfinkel@gmail.com>
2013-02-05 17:42:13 -0800

    blah

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var players = require('./players');
 var drafts = require('./drafts');
+var formats = require('./formats');
 var util = require('./util');
 
 console.log('Starting server.');
@@ -11,24 +12,6 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-function addFormatPage (header, response) {
-
-    var body = '<html><head><title>Add Format</title>\n' +
-	util.randomStyle()+
-    	'</head><body>' +
-	'<form name="the-form" action="/add-format" method="post">\n' +
-	'<center><table bgcolor=white><tr><td align=center><hr><h2><marquee>'+
-	header+
-	'</marquee></h2><hr>\n' +
-	'<tr><td align=center><input type="text" name="format" value="">\n' +
-	'<tr><td align=center><BR><input type="submit" value="Add Format"><input type="reset" value="Clear">' +
-	'<hr></form></table></center></body></html>';	
-
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
-}
 
 app.get('/winners', function(request, response) {
     var b = '<html><head><title>We Love Magic: the Gathering</title>'+util.randomStyle()+'</head><body>\n' +
@@ -56,23 +39,9 @@ app.get('/winners', function(request, response) {
     response.end();
 });
 
-app.get('/add-format', function(request, response) {
-    addFormatPage('Add Format!', response);
-});
-
-app.post('/add-format', function(request, response) {
-    util.readPostData(request, function(body) { 
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-	    var query = client.query('INSERT INTO formats (format) VALUES (\''+body['format']+'\');');	
-	    query.on('end', function(row) { 
-		addFormatPage('Added '+body['format']+' Format! Add Another?', response);
-	    });
-	});
-    });
-});
-
 
 players.setup(app);
 drafts.setup(app);
+formats.setup(app);
 
 console.log('Finished loading server');
