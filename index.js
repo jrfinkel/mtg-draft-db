9c320cfa0f8395e1020e8ffa119ab11ddfb311a4
Jenny Rose Finkel <jrfinkel@gmail.com>
2013-02-05 16:09:10 -0800

    blah

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ app.listen(port, function() {
 app.get('/winners', function(request, response) {
     var b = '<html><head><title>We Love Magic: the Gathering</title>'+util.randomStyle()+'</head><body>\n' +
 	'<center><table><tr><td>' +
-	'Do Stuff<BR>' + 
-	'<a href="./start-draft">Start a Draft</a><BR>' + 
-	'<a href="./add-player">Add a Player</a><BR>' + 
-	'Look at Stuff<BR>' + 
+	'<HR><H2>Do Stuff</H2><HR>' + 
+	'<a href="./start-draft">Start Draft</a><BR>' + 
+	'<a href="./add-player">Add Player</a><BR>' + 
+	'<a href="./add-format">Add Format</a><BR>' + 
+	'<hr><h2>Look at Stuff</h2><hr>' + 
 	'<a href="./all-players?order_by=rating+DESC">Players (by Rating)</a><BR>' + 
 	'<a href="./all-players?order_by=money+DESC">Players (by Money)</a><BR>' + 
 	'<a href="./all-players?order_by=latest_timestamp+DESC">Players (by Recent Activity)</a><BR>' + 
@@ -27,7 +28,7 @@ app.get('/winners', function(request, response) {
 	'<a href="./all-players?order_by=ind_wins+DESC">Players (by Individual Wins)</a><BR>' + 
 	'<a href="./all-players?order_by=ind_losses+DESC">Players (by Individual Losses)</a><BR>' + 
 	'<a href="./all-players?order_by=draft_wins+DESC">Players (by Team Wins)</a><BR>' + 
-	'<a href="./all-players?order_by=draft_losses+DESC">Players (by Losses)</a><BR>' + 
+	'<a href="./all-players?order_by=draft_losses+DESC">Players (by Team Losses)</a><BR>' + 
 	'<a href="./add-player">All Matches</a><BR>' + 
 	'<a href="./add-player">All Drafts</a><BR>' + 
 	'</table></body></html>';
