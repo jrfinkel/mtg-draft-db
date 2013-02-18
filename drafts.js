81618c52fc41b4ae894e2456e52bcf3fda48354e
Jenny Rose Finkel <jrfinkel@gmail.com>
2013-02-17 22:59:53 -0800

    blah

--- drafts.js
+++ drafts.js
@@ -268,7 +268,7 @@ function finalStep (body, response) {
 		tieDiff++;
 	    } else {
 		b += '<tr><td align=center colspan=3><hr><h2><marquee>Losers</marquee><h2><hr>';
-		money = -20;
+		money = -20; 
 		loseDiff++;
 	    }
 	    
