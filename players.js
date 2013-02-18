40b3d615ea2b397f1321f1c6e7e3ce31abb019f6
Jenny Rose Finkel <jrfinkel@gmail.com>
2013-02-17 21:01:52 -0800

    blah

--- players.js
+++ players.js
@@ -137,20 +137,20 @@ function playerInfo(request, response) {
 
 					  body += '<BR><BR><table><tr><th>Match ID<th>Draft ID'+
 					      '<th>Winner<th>Winner Team<th>Winner Post Rating'+
-					      '<th>Loser<th>Loser Team<th>Loser Post Rating<th>Date'+
+					      '<th>Loser<th>Loser Team<th>Loser Post Rating<th>Date';
 
-					  body += JSON.stringify(matches);
+//					  body += JSON.stringify(matches);
 					  
 
-//					  matches.forEach(function(match) {
-//					      body += '<tr><td>'+JSON.stringify(match);
+					  matches.forEach(function(match) {
+					      body += '<tr><td>'+JSON.stringify(match);
 					      //[match.id, match.draft_id, 
 					      // match.winner_name, match.winner_team_id, match.winner_end_rating,
 					      // match.loser_name, match.loser_team_id, match.loser_end_rating,
 					      // match.timestamp_utc].forEach(function(v) {
 						//   body += '<td>'+v;
 					       //});						  
-//					  });
+					  });
 
 					  body += '</table></body></html>';
 
